60007deb4b53aab5c94272d94ff0ef1a368484d7
Sang Yoon <sangyun0904@gmail.com>
2022-12-13 00:21:24 +0900

    add types practice

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 void main() {
   print('hello world');
 
-  // Variables
+  // *** Variables ***
 
   // dynamic : can contain any types of variables
   dynamic dyna_val = "Sangyoon";
@@ -17,12 +17,6 @@ void main() {
   var var_name = "Sangyoon";
   // var_name = 27; // Error : A value of type 'int' can't be assigned to a variable of type 'String'.
 
-
-  String name = "Sangyoon";
-  int age = 27;
-  bool man = true; 
-  double pi = 3.14; // etc
-
   // null safety
   // Null safety prevents errors that result from unintentional access of variables set to null.
   // variables are ‘non-nullable’ by default you can change it nullable by adding '?'
@@ -32,18 +26,79 @@ void main() {
   if (val_nullable != null) {
     val_nullable.length;
   }
-  
+
   // Final Variable
   final double fin_val = 3.14;
   // fin_val = 1; // Error : The final variable 'fin_val' can only be set once.
 
-  // constant variable 
+  // constant variable
   // You can’t change the value of a const variable.
   // Use const for variables that you want to be compile-time constants : const variable should already have value before the compile?
   const const_val = "value";
+
+  // *** Types ***
+
+  String name = "Sangyoon";
+  int age = 27;
+  bool man = true;
+  double pi = 3.14;
+  num x = 12; // rarely used
+  x = 1.1;
+
+  // Lists
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4,
+    // "hello" // Error : The element type 'String' can't be assigned to the list type 'int'.
+  ];
+  // numbers.add('kjshdf'); // Error : The argument type 'String' can't be assigned to the parameter type
+  var giveMeFive = true;
+  var numbers2 = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5, // Collection if
+  ];
+
+  // String Interpolation 
+  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}, nice to meet you";
+  print(greeting);
+
+  // Collection For 
+  var oldFriends = ['Sangyoon', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren', 
+    for(var frind in oldFriends) "good Old friend $frind",
+  ];
+  print(newFriends);
+
+  // Maps 
+  var player = {
+    'name' : 'Sangyoon',
+    'xp' : 19.99,
+    'superpower' : false,
+  };
+  Map<int, String> stringMap = {
+    1: "1",
+    2: "2", 
+    3: "3",
+    // 4: true // Error : The element type 'bool' can't be assigned to the map value type 'String'.
+  };
+
+  // Sets 
+  var numbers3 = {1, 2, 3, 4, 1};
+  numbers3.add(1);
+  numbers3.add(3);
+  numbers3.add(99);
+  print(numbers3);
 }
 
-// Late Variable 
+// Late Variable
 // Declaring a non-nullable variable that’s initialized after its declaration.
 // Lazily initializing a variable.
 // String key; // Error : The non-nullable variable 'key' must be initialized.
