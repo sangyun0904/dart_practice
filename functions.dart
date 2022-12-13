6a50139f44e3a416190c586669e2df3e9e7dd7fd
Sang Yoon <sangyun0904@gmail.com>
2022-12-13 20:52:31 +0900

    modify functions.dart add QQ Operator and typedef

--- functions.dart
+++ functions.dart
@@ -29,6 +29,25 @@ String sayHello3(
   [String? country = 'wakanda']
 ) => "Hello $name, you are $age, and you came from $country";
 
+// QQ Operator
+//String capitalizeName(String? name) => name != null ? name.toUpperCase() : 'ANON';
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
+
+typedef ListOfInts = List<int>;
+
+// List<int> reverseListOfNumbers(List<int> list) {
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+typedef UserInfo = Map<String, String>;
+
+// String sayHi(Map<String, String> userInfo) {
+String sayHi(UserInfo userInfo) { 
+  return "Hi ${userInfo['name']}";
+}
+
 void main() {
   print(sayHello2(
     name: "Sangyoon",
@@ -36,4 +55,14 @@ void main() {
     country: "Korea",
   ));
   print(sayHello3('name', 27));
+  print(capitalizeName("kim"));
+  print(capitalizeName(null));
+
+  String? name;
+  name ??= 'ANON';
+  //name ??= 'another'; // Warning: Operand of null-aware operation '??=' has type 'String' which excludes null.
+  name = null;
+  name ??= 'another';
+
+  print(reverseListOfNumbers([1,2,3]));
 }
