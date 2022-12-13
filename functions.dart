ab7069c9be7c959f72a34c8cde52233e76d1747d
Sang Yoon <sangyun0904@gmail.com>
2022-12-13 15:53:52 +0900

    add function.dart

---
+++ functions.dart
@@ -0,0 +1,8 @@
+String sayHello(String name) {
+  return "Hello $name nice to meet you!";
+}
+// String sayHello(String name) => "Hello $name nice to meet you!";
+
+void main() {
+  print(sayHello("Sangyoon"));
+}
